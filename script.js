56944a6f3b3bc3da222d410de56edf791f017995
Giancyril <mijaresgiancyril@gmail.com>
2025-03-13 17:11:50 +0800

    fixed the show and hide password

--- script.js
+++ script.js
@@ -14,52 +14,37 @@ const form = document.querySelector("form"),
 function checkname() {
   const namePattern = /^(?=.*[a-z])/;
   if (!nameInput.value.match(namePattern)) {
-    return nameField.classList.add("invalid"); //adding invalid class if email value do not mathced with email pattern
+    return nameField.classList.add("invalid"); 
   }
-  nameField.classList.remove("invalid"); //removing invalid class if email value matched with emaiPattern
+  nameField.classList.remove("invalid"); 
 }
 
   // Username Validtion
 function checkusername() {
   const usernamePattern = /^(?=.*[a-z]{2,3}$)/;
   if (!usernameInput.value.match(usernamePattern)) {
-    return usernameField.classList.add("invalid"); //adding invalid class if email value do not mathced with email pattern
+    return usernameField.classList.add("invalid"); 
   }
-  usernameField.classList.remove("invalid"); //removing invalid class if email value matched with emaiPattern
+  usernameField.classList.remove("invalid"); 
 }
 
 // Email Validtion
 function checkEmail() {
   const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
   if (!emailInput.value.match(emaiPattern)) {
-    return emailField.classList.add("invalid"); //adding invalid class if email value do not mathced with email pattern
+    return emailField.classList.add("invalid"); 
   }
-  emailField.classList.remove("invalid"); //removing invalid class if email value matched with emaiPattern
+  emailField.classList.remove("invalid"); 
 }
-
-// Hide and show password
-const eyeIcons = document.querySelectorAll(".show-hide");
-
-eyeIcons.forEach((eyeIcon) => {
-  eyeIcon.addEventListener("click", () => {
-    const pInput = eyeIcon.parentElement.querySelector("input"); //getting parent element of eye icon and selecting the password input
-    if (pInput.type === "password") {
-      eyeIcon.classList.replace("bx-hide", "bx-show");
-      return (pInput.type = "text");
-    }
-    eyeIcon.classList.replace("bx-show", "bx-hide");
-    pInput.type = "password";
-  });
-});
 
 // Password Validation
 function createPass() {
   const passPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
   if (!passInput.value.match(passPattern)) {
-    return passField.classList.add("invalid"); //adding invalid class if password input value do not match with passPattern
+    return passField.classList.add("invalid");
   }
-  passField.classList.remove("invalid"); //removing invalid class if password input value matched with passPattern
+  passField.classList.remove("invalid"); 
 }
 
 // Confirm Password Validtion
@@ -69,4 +54,3 @@ function confirmPass() {
   }
   cPassField.classList.remove("invalid");
 }
-
